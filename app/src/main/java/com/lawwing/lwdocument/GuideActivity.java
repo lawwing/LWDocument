9dc078297a529784be45a86aba1284064482f7a3
lzr <123456>
2017-11-15 14:08:43 +0800

    修改了APP的导航页

--- app/src/main/java/com/lawwing/lwdocument/GuideActivity.java
+++ app/src/main/java/com/lawwing/lwdocument/GuideActivity.java
@@ -1,6 +1,7 @@
 package com.lawwing.lwdocument;
 
 import com.lawwing.lwdocument.base.BaseActivity;
+import com.lawwing.lwdocument.utils.SystemUtils;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -8,9 +9,11 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.text.TextUtils;
 import android.util.Log;
+import android.widget.TextView;
 
 public class GuideActivity extends BaseActivity
 {
+    private TextView appNameText;
     
     private String filePath;
     
@@ -20,6 +23,7 @@ public class GuideActivity extends BaseActivity
         super.onCreate(savedInstanceState);
         getIntentData();
         setContentView(R.layout.activity_guide);
+        appNameText = (TextView) findViewById(R.id.appNameText);
         // 每隔100毫秒执行onTick中的方法一次
         // 直到执行完2000/1000次为止，最后会执行onFinish()中的方法
         CountDownTimer countDownTimer = new CountDownTimer(2000, 1000)
